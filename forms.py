c9d6b5f764f07909281f5e054b88f088b7524b6d
Blazej Nowicki <blazej.a.now@gmail.com>
2021-11-05 11:32:16 +0100

    new configuration site

--- forms.py
+++ forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired
 
 class EditFrom(FlaskForm):
     time = TimeField('Time', validators=[DataRequired()])
-    playlist_name = StringField('Playlist\'s name:', validators=[DataRequired()])
-    playlist_id = StringField('Playlist\'s URI: ', validators=[DataRequired()])
+    playlist_name = StringField('Playlist\'s name', validators=[DataRequired()])
+    playlist_id = StringField('Playlist\'s URI', validators=[DataRequired()])
     submit = SubmitField('Done')
